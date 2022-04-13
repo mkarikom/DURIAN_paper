a95c08aba09736b53b5d70ba269082cb33978d01
Matt Karikomi <mattkarikomi@gmail.com>
2022-04-13 09:47:15 -0700

    initial

--- slurm/He/run_durian_outerMetrics_multi_1K05.sh
+++ slurm/He/run_durian_outerMetrics_multi_1K05.sh
@@ -13,12 +13,12 @@ prefix=$dsname
 SLURMACCT=qnie_lab
 
 export EMDIAG=FALSE
-export SLURMPARTITION=highmem
+export SLURMPARTITION=free
 export slurmtimelimit=0-00:30:00
 export PROJECTDIR=/dfs6/pub/mkarikom/code/DURIAN_paper_clean
 export BASEDIR=$PROJECTDIR/slurm
 export OUTPUTMASTER=$BASEDIR/${dsname}/output.clusterMetrics.$SLURMPARTITION.$suffix
-export NCPUS=22
+export NCPUS=5
 
 MEMP=16000M # memory in mb, try increasing if nodes are not avail
 export SOURCEPATH=$BASEDIR/${dsname}/durian_data
